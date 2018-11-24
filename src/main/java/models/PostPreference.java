e30db9b66adfc3eb7a1a728c7a62a13d443a2d5d
rbuck01 <44786570+rbuck01@users.noreply.github.com>
2018-11-24 01:34:12 -0500

    Some rough drafts
    just to give an idea of what I'm thinking

---
+++ src/main/java/models/PostPreference.java
@@ -0,0 +1,29 @@
+//imports
+
+
+//bean
+
+@Component
+public class PostPreference	{
+
+//@Id
+private int postID;
+
+//foreign key
+private String userID;
+
+
+
+private boolean gluten;
+private boolean peanut;
+private boolean vegetarian;
+
+//many more preferences
+
+
+
+//constructors
+//getters, setters
+//toString
+
+}
---
+++ src/main/java/models/Posting.java
@@ -0,0 +1,22 @@
+//	imports
+
+
+//	bean
+
+
+@Component
+public class Posting	{
+
+
+
+private String postTitle;
+private String postBody;
+
+
+
+
+//constructors
+//getters, setters
+//toString
+
+}
