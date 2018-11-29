eb0a8127d36de4871acb3644d3162fb5a60a1b1c
Donny LaPlume <donnylaplume@gmail.com>
2018-11-29 16:45:01 -0500

    Login attempt 6

--- LunchMate/src/main/java/com/revature/controllers/HomeController.java
+++ LunchMate/src/main/java/com/revature/controllers/HomeController.java
@@ -12,25 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.daos.UserDao;
+import com.revature.daos.UserDaoImp;
 import com.revature.models.User;
 import com.revature.services.UserService;
 
 
 
-@Controller
+@RestController
 @RequestMapping("login")
 public class HomeController {
 
-	@RequestMapping(method=RequestMethod.GET, value="/home")
-public String getHomePage()
-	{	return "Home";	}
-
-
-	@GetMapping("/")
-	public String goLogin()
-	{	return "Login";	}
-
+	UserDao udao = new UserDaoImp();
+	
 	@PostMapping
 	public User getLogin(@RequestBody User u)
 	{	
@@ -40,44 +36,12 @@ public String getHomePage()
 		{
 			if(user.get(i).getUsername().equals(u.getUsername()) && user.get(i).getPassword().equals(u.getPassword()))
 			{
-				return user.get(i);
+				//.out.println("confirm");
+				return udao.getUser(user.get(i).getId());
 			}
 		}
 		
 		return u;	
 	}
-
-
-
-	// Grab posting preferences by ID
-
-	// Respond to a posting
-
-	
-	@Autowired
-	UserService userService;
-	
-
-
-/*
-//	I HAVE NO IDEA IF THIS WOULD WORK
-	@PostMapping("/login")
-	public String loggedInUser(@RequestParam("username") String user,
-	@RequestParam("password")String pass) {
-		if (userService.getUserLogin(user, pass)) {
-			return "Home";
-		}
-		return "";
-}
-*/
-
-
-
-
-
-
-
-
-
 
 }
